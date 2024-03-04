279f6ce96a2a22d81f09abc0f7972ed1c6fff70f
brentnettles <brentnettles@gmail.com>
2024-03-04 15:32:03 -0500

    Comps  outlined

---
+++ src/assets/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+        NavBar
+        <div className='links'>
+            <Link to="./home">Home</Link>
+            <Link to="./memeLibrary">Library<Link>
+            <Link to="./favorites">Favorites</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar;
