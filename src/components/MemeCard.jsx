87b560331994121926de57230349f607739c460f
brentnettles <brentnettles@gmail.com>
2024-03-05 13:47:11 -0500

    NewMemeForm + POST

--- src/components/MemeCard.jsx
+++ src/components/MemeCard.jsx
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom';
 
 function MemeCard({ meme }) {
   return (
-    <div>
+   <>
+   <div>
        {/* <NavBar /> */}
       {/* <h2>add like button & edit option to update tags</h2> */}
-      <h2>[*]]</h2>
+      <h2></h2>
       <Link to={`/memes/${meme.id}`}>
         <img src={meme.image} alt={meme.title || 'Meme'} />
         <br/><button>❤️</button> <br/>
         <button>EDIT</button>
       </Link>
     </div>
+    </>
   );
 }
 
