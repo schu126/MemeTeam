42d02392d4902bb242307518e161794d620cf045
brentnettles <brentnettles@gmail.com>
2024-03-07 16:43:09 -0500

    Phase-2 Project Memeteam! Missing small css updates to Favorites + Like Button State issue on Home / MemeMe page

--- src/pages/MemeLibrary.jsx
+++ src/pages/MemeLibrary.jsx
@@ -3,11 +3,10 @@ import MemeCard from '../components/MemeCard';
 import NavBar from '../components/NavBar';
 import { Link } from 'react-router-dom';
 
-
-
 function MemeLibrary() {
     const [memes, setMemes] = useState([]);
     const [error, setError] = useState(null);
+    const [searchedMeme, setSearchedMeme] = useState('');
 
     useEffect(() => {
         fetch('http://localhost:3000/memes')
